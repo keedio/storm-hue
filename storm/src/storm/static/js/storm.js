637b28e8be54363ce287c86c9fcc257a0fce26a3
jjmleiro <jjmartinez@keedio.com>
2015-01-16 08:05:40 +0100

    Delete unnecesary files

--- storm/src/storm/static/js/storm.js
+++ storm/src/storm/static/js/storm.js
@@ -1,3 +1,25 @@
+function changeDisplay(id1, id2) {
+   document.getElementById(id1).style.display = "";
+   document.getElementById(id2).style.display = "none";            
+};
+   
+function changeTopologyStatus(psId, psAction, pbWait, piWait) {            
+   if (confirm('Are you sure you want too '  + psAction +  ' this Topology?')) {
+      // Accept.
+      $.post("/storm/changeTopologyStatus/", { sId: psId,
+                                               sAction: psAction,
+   	                                           bWait: pbWait,
+	                                           iWait: piWait
+                                             }, function(data){                                                    
+                                                   if (data = 200) {
+                                                      window.location.reload();
+                                                   }
+                                                }, "text");
+   } 
+   else {
+      // Cancel.
+   }         
+};
 
 function StormViewModel() {
    var self = this;         
